682602e2607674295845c7712489680318889c37
ihortok <itokaryk@gmail.com>
2021-01-26 20:25:24 +0200

    players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,15 @@
 class PlayersController < ApplicationController
-  def index; end
+  before_action :set_player, only: :show
+
+  def index
+    @players = Player.all
+  end
 
   def show; end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ApplicationRecord
   belongs_to :participant
 
   as_enum :position, goalkeeper: 0, defender: 1, midfielder: 2, attaker: 3
+
+  def age
+    Time.current.year - participant.date_of_birth.year
+  end
 end
