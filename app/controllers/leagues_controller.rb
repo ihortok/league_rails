04501dc9d98e7901445d642fd0b330b9ed869a01
ihortok <itokaryk@gmail.com>
2021-01-26 20:12:46 +0200

    league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,15 @@
 class LeaguesController < ApplicationController
-  def index; end
+  before_action :set_leagues, only: :show
+
+  def index
+    @leagues = League.all
+  end
 
   def show; end
+
+  private
+
+  def set_leagues
+    @league = League.find(params[:id])
+  end
 end
